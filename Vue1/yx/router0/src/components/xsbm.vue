1d002fa74ca95296766ebf82a1c15a97f5215899
baby00700 <1014504021@qq.com>
2017-09-13 14:41:12 +0800

    yx--vue

---
+++ Vue1/yx/router0/src/components/xsbm.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <p>{{msg}}</p>
+  <input v-model='msg' id='input1' v-bind:disabled='disabled ? true : false' />
+  <button id='sub1' @click='alert0(msg)' v-bind:disabled='disabled ? true : false'>submit</button>
+</div>
+  
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      msg: '新生报名22',
+      disabled: true
+    }
+  },
+  methods: {
+    getdata: function () {
+      console.log('456')
+    },
+    alert0: function (msg) {
+      alert(msg)
+    }
+  }
+}
+</script>
+<style>
+#input1{
+  border:1px solid red;
+}
+</style>
